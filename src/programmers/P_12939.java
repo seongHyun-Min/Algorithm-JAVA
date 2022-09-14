f07e8ef729a09dc4fd323e992e98093bcc927963
seougHyun Min <steve6133@naver.com>
2022-09-14 10:23:53 +0900

    프로그래머스 연습문제 (최대값과 최솟값)

---
+++ src/programmers/P_12939.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.io.IOException;
+
+public class P_12939 {
+        public static String solution(String s) {
+            String str[] = s.split("\\s");
+            Integer max =Integer.MIN_VALUE;
+            Integer min =Integer.MAX_VALUE;
+            for(int i=0; i<str.length; i++){
+                int tmp = Integer.parseInt(str[i]);
+                max = Math.max(max, tmp);
+                min = Math.min(min, tmp);
+            }
+
+            String answer = String.valueOf(min)+" "+String.valueOf(max);
+            return answer;
+        }
+
+    public static void main(String[] args) throws IOException { //테스트 코드
+        System.out.println(solution("1 -2 3 4"));
+
+
+    }
+}
