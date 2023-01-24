ab61484e75d8d07dd5673cae58112e31f0e4f70d
seougHyun Min <steve6133@naver.com>
2023-01-25 00:38:44 +0900

    백준 18352번 특정 거리의 도시 찾기(BFS,최단거리)

--- src/B_18352.java
+++ src/B_18352.java
@@ -1,71 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.*;
 
-public class B_18352 {
+    public class B_18352 {
 
-    static int N, M, K, X;
+        static int N, M, K, X;
 
-    static List<Integer> answer; // 정답 배열 선언
-
-
-    static ArrayList<ArrayList<Integer>> arr;
+        static List<Integer> dist;
 
 
-    static int[] visited; // 방문 배열을 int로 선언 (가중치를 넣기 위해서)
+        static ArrayList<Integer>[] arr;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 도시의 개수 (노드의 수)
-        M = Integer.parseInt(st.nextToken()); // 도로의 개수 (엣지의 수)
-        K = Integer.parseInt(st.nextToken()); // 가중치 K의 값
-        X = Integer.parseInt(st.nextToken()); // 출발도시 X
 
-        arr = new ArrayList<>();
-        for (int i = 1; i <= N + 1; i++) {
-            arr.add(new ArrayList<>());
-        }
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+        static int[] answer;
 
-            arr.get(u).add(v);
-            // 단방향 노드 삽입
-        }
-        visited = new int[N + 1];
-        Arrays.fill(visited, -1); //방문 배열 초기화
-        bfs(X);
+        static boolean[] visited;
 
-        answer = new ArrayList<>();
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); //노드(도시의 개수)
+            M = Integer.parseInt(st.nextToken()); //엣지(도로의 개수)
+            K = Integer.parseInt(st.nextToken()); //거리 정보
+            X = Integer.parseInt(st.nextToken()); //출발 도시 정보
 
-        for (int i = 1; i < visited.length; i++) {
-            if (visited[i] == K) answer.add(i);
-        }
+            arr = new ArrayList[N + 1];
+            dist = new ArrayList<>();
+            for(int i=1; i<=N; i++){
+                arr[i] = new ArrayList<>(); //인접 리스트 생성
+            }
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken()); //출발 도시
+                int B = Integer.parseInt(st.nextToken()); //도착 도시
+                arr[A].add(B);
+            }
+            visited = new boolean[N+1];
+            answer = new int[N+1];
 
-        if (answer.size() > 0) {
-            Collections.sort(answer);
-            answer.forEach(System.out::println);
-        } else System.out.println(-1);
-    }
+            BFS(X);
 
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start]++;
-        while (!q.isEmpty()) {
-            int tmp = q.poll();
-            for (int i = 0; i < arr.get(tmp).size(); i++) {
-                int next = arr.get(tmp).get(i);
-                if (visited[next] == -1) {
-                    visited[next] = visited[tmp] + 1;
-                    q.add(next);
+            for(int i=1; i<=N; i++) {
+                if (answer[i] == K) {
+                    dist.add(i);
                 }
             }
+            //출력
+            if(dist.isEmpty()){
+                System.out.println(-1);
+            }
+            else{
+                Collections.sort(dist);
+                dist.forEach(System.out::println);
+            }
+        }
+        public static void BFS(int x){
+             Queue<Integer> q = new LinkedList<>();
+             q.add(x);
+             visited[x] =true; //기존 visited[x] ++와 같은 코드
+             while(!q.isEmpty()){
+                 int current = q.poll();
+                 for(int i : arr[current]){
+                     if(answer[i] == 0 && !visited[i]){
+                         answer[i] = answer[current] +1;
+                         visited[i] = true; //방문한 노드 방문처리
+                         q.offer(i);
+                     }
+                 }
+             }
         }
     }
-}
-
-
